b41580093c48a3c96f430b622e040b02539e533b
Miguel Castro <miguel.castro@lvdigital.de>
2021-08-13 08:29:07 +0200

    search from and filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -42,6 +42,28 @@ class Post extends Model
 
     protected $guarded = [];
 
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+        );
+
+
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query
+                // ->whereExists(fn($query) =>
+                //     $query->from('categories')
+                //         ->whereColumn('categories.id', 'posts.category_id')
+                //         ->where('categories.slug', $category))
+                // SIMPLER WAY
+                ->whereHas('category', fn ($query )=> 
+                 $query->where('slug', $category)
+                )
+            );
+    }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -18,28 +19,18 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
-    return view('posts', [
-        'posts' => Post::latest()->with(['category', 'author'])->get(),
-        'categories' => Category::all()
-    ]);
-});
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('posts/{post:slug}', function (Post $post) {
-    return view('post', [
-        'post' => $post
 
-    ]);
-});
+// Route::get('categories/{category:slug}', function (Category $category) {
+//     return view('posts', [
+//         'posts' => $category->posts->load(['category', 'author']),
+//         'categories' => Category::all(),
+//         'currentCategory' => $category
 
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts->load(['category', 'author']),
-        'categories' => Category::all(),
-        'currentCategory' => $category
-
-    ]);
-});
+//     ]);
+// });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [

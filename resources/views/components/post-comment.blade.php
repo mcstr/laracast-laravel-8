e257a56a0e5ff3bb4c43aeaad5b948d5db53ab25
Miguel Castro <miguel.castro@lvdigital.de>
2021-08-30 10:38:57 +0200

    add relationships from comment to user and to post

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,38 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img class="rounded-full" src="https://i.pravatar.cc/60" alt="" width="60" height="60">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted <time>8 months ago</time>
+            </p>
+        </header>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem in nisi molestias, vitae
+            officiis exercitationem id! Sapiente impedit veniam aliquid similique illo ipsa omnis quas
+            optio! Rem doloremque quaerat minima!
+            Distinctio cupiditate dignissimos eum quas doloremque earum quos nesciunt esse similique
+            illo provident, ut, ducimus recusandae, suscipit placeat enim fuga! Delectus assumenda
+            expedita pariatur laudantium ratione provident quidem impedit possimus!
+            Eius impedit quia reprehenderit illo consequuntur animi consectetur in tenetur unde hic
+            neque repellendus corrupti possimus incidunt ad pariatur, amet est delectus quod! Quia sunt
+            est, voluptate nemo iure commodi!
+            Ipsum modi excepturi nostrum quae explicabo ex, recusandae laboriosam nam expedita saepe
+            voluptate libero laborum necessitatibus, laudantium dolorem minus maiores deserunt adipisci
+            accusantium vel, eaque quos amet esse? Maiores, sapiente?
+            Nulla dignissimos incidunt consequuntur aperiam saepe rem expedita quo, nihil aut quam amet
+            iste adipisci exercitationem ipsam neque cumque culpa vitae maxime repellat dolorum! Nulla
+            inventore neque ipsam laborum recusandae!
+            Iure sint quo eveniet eaque doloremque tempore tempora alias autem, rem pariatur ipsam
+            ratione id tenetur nobis beatae blanditiis temporibus in corporis distinctio nisi. Ratione
+            incidunt expedita error ipsa et.
+            Minima molestias dolor porro voluptatibus facere eum, omnis est recusandae deserunt,
+            quibusdam exercitationem necessitatibus tempora, nobis velit. Ducimus similique vero
+            sapiente illo eaque illum optio fugit pariatur. Optio, dolore qui.
+            Incidunt architecto doloribus voluptate distinctio, ullam facere quisquam fugit nisi autem
+            consectetur, asperiores dignissimos nostrum natus, eveniet iure illo iusto numquam atque
+            aperiam provident? Tenetur harum corporis atque earum nostrum.
+        </p>
+    </div>
+</article>
